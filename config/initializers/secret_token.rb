a3ae7c9fd7f4ada105bfe1da54d835d97c705d3c
Zxer0 <matt@kitcheck.com>
2016-07-27 09:42:51 -0400

    kick off the mattogram project like a boss

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mattogram::Application.config.secret_token = '51f61a500eb0cedf1d3e07368e6e3560a1924a72c9f6df2448a08a101a197185d76bbdd15563de71a7de88d7fdadab7bf863065e58b49059bffe646dd10b1e05'
